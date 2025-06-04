7709e12f1ae110c6a8b5a75ef9df78e249058323
Uncle Dandry <87745515+Uncle-Dandry@users.noreply.github.com>
2025-06-04 16:55:02 +0300

    Fix shape mismatch in banded solver

--- smooth_path.py
+++ smooth_path.py
@@ -74,11 +74,11 @@ def smooth_path(initial_path, vertices_build, faces_build, obstacle_heights):
   y_initial = initial_path[:, 1]
   z_initial = initial_path[:, 2]
 
-  N = len(x_initial) - 2
+  num_inner = len(x_initial) - 2
   acceptable_deviation = 1.5
   delta_steps = 300
   delta_increment = 1 / delta_steps
-  num_points = N - 2
+  num_points = num_inner
   
   x_optimized = []
   y_optimized = []
@@ -96,7 +96,10 @@ def smooth_path(initial_path, vertices_build, faces_build, obstacle_heights):
       if num_points > 2:
         main_diag = np.full(num_points, diag_value)
         off_diag = np.full(num_points - 1, -delta2)
-        A_banded = np.vstack((np.append(0, off_diag), main_diag, np.append(off_diag, 0)))
+        A_banded = np.zeros((3, num_points))
+        A_banded[0, 1:] = off_diag
+        A_banded[1] = main_diag
+        A_banded[2, :-1] = off_diag
 
         B_x = delta1 * x_initial[1:-1]
         B_y = delta1 * y_initial[1:-1]
@@ -104,33 +107,33 @@ def smooth_path(initial_path, vertices_build, faces_build, obstacle_heights):
         B_x[0] += delta2 * x_initial[0]
         B_y[0] += delta2 * y_initial[0]
         B_z[0] += delta2 * z_initial[0]
-        B_x[-1] += delta2 * x_initial[N - 1]
-        B_y[-1] += delta2 * y_initial[N - 1]
-        B_z[-1] += delta2 * z_initial[N - 1]
+        B_x[-1] += delta2 * x_initial[-1]
+        B_y[-1] += delta2 * y_initial[-1]
+        B_z[-1] += delta2 * z_initial[-1]
 
         inner_x = solve_banded((1, 1), A_banded, B_x)
         inner_y = solve_banded((1, 1), A_banded, B_y)
         inner_z = solve_banded((1, 1), A_banded, B_z)
 
-        x_optimized = np.concatenate(([x_initial[0]], inner_x, [x_initial[N - 1]]))
-        y_optimized = np.concatenate(([y_initial[0]], inner_y, [y_initial[N - 1]]))
-        z_optimized = np.concatenate(([z_initial[0]], inner_z, [z_initial[N - 1]]))
+        x_optimized = np.concatenate(([x_initial[0]], inner_x, [x_initial[-1]]))
+        y_optimized = np.concatenate(([y_initial[0]], inner_y, [y_initial[-1]]))
+        z_optimized = np.concatenate(([z_initial[0]], inner_z, [z_initial[-1]]))
       else:
         A = np.array([[diag_value, -delta2], [-delta2, diag_value]])
-        B_x = np.array([delta1 * x_initial[1] + delta2 * x_initial[0], delta1 * x_initial[2] + delta2 * x_initial[3]])
-        B_y = np.array([delta1 * y_initial[1] + delta2 * y_initial[0], delta1 * y_initial[2] + delta2 * y_initial[3]])
-        B_z = np.array([delta1 * z_initial[1] + delta2 * z_initial[0], delta1 * z_initial[2] + delta2 * z_initial[3]])
+        B_x = np.array([delta1 * x_initial[1] + delta2 * x_initial[0], delta1 * x_initial[-2] + delta2 * x_initial[-1]])
+        B_y = np.array([delta1 * y_initial[1] + delta2 * y_initial[0], delta1 * y_initial[-2] + delta2 * y_initial[-1]])
+        B_z = np.array([delta1 * z_initial[1] + delta2 * z_initial[0], delta1 * z_initial[-2] + delta2 * z_initial[-1]])
 
-        x_optimized = np.concatenate(([x_initial[0]], np.linalg.solve(A, B_x), [x_initial[N - 1]]))
-        y_optimized = np.concatenate(([y_initial[0]], np.linalg.solve(A, B_y), [y_initial[N - 1]]))
-        z_optimized = np.concatenate(([z_initial[0]], np.linalg.solve(A, B_z), [z_initial[N - 1]]))
+        x_optimized = np.concatenate(([x_initial[0]], np.linalg.solve(A, B_x), [x_initial[-1]]))
+        y_optimized = np.concatenate(([y_initial[0]], np.linalg.solve(A, B_y), [y_initial[-1]]))
+        z_optimized = np.concatenate(([z_initial[0]], np.linalg.solve(A, B_z), [z_initial[-1]]))
 
-      if len(x_optimized) != len(x_initial) - 2:
+      if len(x_optimized) != len(x_initial):
         continue
 
       deviation = np.sqrt(
-        np.sum((x_optimized - x_initial[1:-1])**2 + (y_optimized - y_initial[1:-1])**2 + (z_optimized - z_initial[1:-1])**2)
-      ) / np.sqrt(N - 2)
+        np.sum((x_optimized[1:-1] - x_initial[1:-1])**2 + (y_optimized[1:-1] - y_initial[1:-1])**2 + (z_optimized[1:-1] - z_initial[1:-1])**2)
+      ) / np.sqrt(num_inner)
 
       if deviation <= acceptable_deviation and not check_path_intersection(vertices_build, faces_build, x_optimized, y_optimized, z_optimized):
         path_found = True
@@ -141,9 +144,15 @@ def smooth_path(initial_path, vertices_build, faces_build, obstacle_heights):
 
         break
 
-    initial_length = np.sum([distance(np.array([x_initial[i], y_initial[i], z_initial[i]]),
-      np.array([x_initial[i + 1], y_initial[i + 1], z_initial[i + 1]])) for i in range(N - 1)])
-    optimized_length = np.sum([distance(np.array([x_optimized[i], y_optimized[i], z_optimized[i]]),
-      np.array([x_optimized[i + 1], y_optimized[i + 1], z_optimized[i + 1]])) for i in range(N - 1)])
+    initial_length = np.sum([
+      distance(np.array([x_initial[i], y_initial[i], z_initial[i]]),
+               np.array([x_initial[i + 1], y_initial[i + 1], z_initial[i + 1]]))
+      for i in range(len(x_initial) - 1)
+    ])
+    optimized_length = np.sum([
+      distance(np.array([x_optimized[i], y_optimized[i], z_optimized[i]]),
+               np.array([x_optimized[i + 1], y_optimized[i + 1], z_optimized[i + 1]]))
+      for i in range(len(x_optimized) - 1)
+    ])
   
   return [x_optimized, y_optimized, z_optimized]
